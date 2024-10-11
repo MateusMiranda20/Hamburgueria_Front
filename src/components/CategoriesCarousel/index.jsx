a12ddbf3d4a6a084f9b55cfcec579c9da26295e6
MateusMiranda20 <mateuusmiranda20@gmail.com>
2024-10-10 23:42:57 -0300

    Criação do carrosel categorias

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react"
+import { Container, Title, ContainerItems } from '../CategoriesCarousel/styles'
+
+
+
 import { api } from "../../services/api"
+import Carousel from "react-multi-carousel";
+import 'react-multi-carousel/lib/styles.css'
+
 
 export function CategoriesCarousel() {
     
@@ -18,9 +25,43 @@ export function CategoriesCarousel() {
         loadCategories()
     }, []);
 
+    const responsive = {
+        superLargeDesktop: {
+          breakpoint: { max: 4000, min: 3000 },
+          items: 4,
+        },
+
+        desktop: {
+          breakpoint: { max: 3000, min: 1280 },
+          items: 4,
+        },
+        tablet: {
+          breakpoint: { max: 1280, min: 690 },
+          items: 3,     
+        },
+        mobile: {
+          breakpoint: { max: 690, min: 0 },
+          items: 2,     
+        },
+      };
+
     return(
-        <div>
-            ok
-        </div>
+        <Container>
+            <Title>Categorias</Title>
+            <Carousel
+                responsive={responsive}
+                infinite={true}
+                partialVisible={false}
+                itemClass="carousel-item"
+            
+            
+            >
+                {categories.map((category) => (
+                    <ContainerItems key={category.id} imageUrl={category.url}>
+                        <p>{category.name}</p>
+                    </ContainerItems>
+                ))}
+            </Carousel>
+        </Container>
     )
 }
--- src/components/CategoriesCarousel/styles.js
+++ src/components/CategoriesCarousel/styles.js
@@ -1,1 +1,58 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+
+    .carousel-item{
+        padding: 40px;
+    }
+`
+
+export const Title = styled.h2`
+    font-size: 30px;
+    font-family: sans-serif;
+    color: #9758a6;
+    font-weight: 800;
+    padding-bottom: 12px;
+    position: relative;
+    text-align: center;
+
+    &::after{
+        content:'' ;
+        position: absolute;
+        bottom: 0;
+        width: 56px;
+        height: 4px;
+        background-color: #9758a6;
+        left: 50%;
+        transform: translateX(-50%);
+        top: 60px;
+    }
+`
+
+export const ContainerItems = styled.div`
+    background: url('${(props) => props.imageUrl}'), no-repeat;
+    background-position: center;
+    background-size: cover;
+    border-radius: 20px;
+
+
+    display: flex;
+    align-items: center;
+    padding: 20px 10px;
+    width: 100%;
+    height: 200px;
+
+    p{
+        color: #fff;
+        background-color: rgba(0, 0, 0, 0.5);
+        padding: 10px 30px;
+        border-radius: 18px;
+        font-size: 20px;
+        font-weight: bold;
+        font-family: sans-serif;
+        margin-top: 100px;
+        text-transform: capitalize; /* Faz com que a primeira letra de cada palavra seja maiúscula */
+}
+
+    }
+`
