7f537a9acf39c7bfb3c770d49b9fc7e66ca7d909
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-01-10 23:20:57 -0300

    Deseenvolvendo menu lateral do admin

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,8 +1,31 @@
+import { NavLink } from 'react-router-dom'
+import Logo from '../../assets/Burguer.png'
+import { navLinks } from "./navLinks"
+import { SignOut } from '@phosphor-icons/react'
+import { Container, NavLinks, NavLinkContainer, Footer } from './styles'
+import { useUser} from '../../hooks/UserContext'
 
 export function SideNavAdmin() {
+    
+    const {logout} = useUser();
+
     return(
-        <div>
-            <h3>Menu</h3>
-        </div>
+        <Container>
+            <img src={Logo} alt="Hamburguer Logo" />
+            <NavLinkContainer>
+               {navLinks.map( link => (
+                <NavLinks key={link.id} to={link.path}>
+                    {link.icon}
+                    <span>{link.label}</span>
+                </NavLinks>
+               ))} 
+            </NavLinkContainer>
+            <Footer>
+                <NavLinks to="/login" onClick={logout}>
+                    <SignOut />
+                    <p>Sair</p>
+                </NavLinks>
+            </Footer>
+        </Container>
     )
 }
