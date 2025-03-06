b778bb331770e4ea37c76f2b284326a3803899b8
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-03-06 14:16:03 -0300

    error axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://devburguer-api.onrender.com' || "http://localhost:4000",
-    withCredentials: true
-  });
+  baseURL: "https://devburguer-api.onrender.com", // URL do backend
+  withCredentials: true, // Se o backend usa cookies de autenticação
+});
 
-  api.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
+  const userData = localStorage.getItem('devburguer:userData');
+  const token = userData && JSON.parse(userData).token;
 
-    const userData =   localStorage.getItem('devburguer:userData');
-
-    const token = userData && JSON.parse(userData).token
-
+  if (token) {
     config.headers.authorization = `Bearer ${token}`;
+  }
 
-    return config
-  })
+  return config;
+});
